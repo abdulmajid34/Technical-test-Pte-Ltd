2ee8730d63c73e4fc9a248736ffd67cb0d426d2f
abdulmajid <majid@kebun.tech>
2023-08-15 12:03:34 +0700

    feat: add laayout card previous order

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,20 +6,20 @@
 
         </div>
       </div>
-      <div class="content-card container">
+      <div class="container" style="margin-top: -10rem;">
         <div class="row">
           <div class="col-md-4">
-            <h2 class="text-white" style="margin-bottom: 16px; font-weight: 500; line-height: 25.11px; font-size: 21.43px;">Welcome, john</h2>
+            <h2 class="text-white mx-4" style="margin-bottom: 16px; font-weight: 500; line-height: 25.11px; font-size: 21.43px;">Welcome, john</h2>
           </div>
         </div>
         <div class="row justify-content-center">
           <div class="col-md-4">            
-            <div class="card shadow--lg" style="border-radius: 10px;">
+            <div class="card shadow-sm mx-4" style="border-radius: 10px; width: 302px; height: 167px; border: none;">
               <div>
-                <img src="../assets/images/Ellipse_1.png" class="card-img-top" alt="elipse_1" style="width: 139px; height: 139px;">
-                <img src="../assets/images/Ellipse_2.png" class="card-img-top" alt="elipse_2" style="width: 29px; padding-left: 9px;">
+                <img src="../assets/images/Ellipse_1.png" alt="elipse_1" style="width: 100px; height: 90px;">
+                <img src="../assets/images/Ellipse_2.png" alt="elipse_2" style="width: 29px; padding-left: 9px;">
               </div>
-              <div class="card-body text-end">
+              <div class="card-body text-end" style="margin-top: -25px">
                 <div class="">
                   <span style="font-size: 18.84px; font-weight: 400; line-height: 22.07px; color: #535353;">Your Balance</span>
                 </div>
@@ -30,7 +30,37 @@
             </div>
           </div>
         </div>
-        
+        <div class="row justify-content-center" style="margin-top: 40px">
+          <div class="col-md-4">
+            <h2 class="mx-4" style="color: #3B97CB; font-weight: 700; font-size: 16.08px; line-height: 18.84px;">PREVIOUS ORDER</h2>
+          </div>
+        </div>
+        <div class="row justify-content-center">
+          <div class="col-md-4">
+            <div class="card mx-4" style="width: 302px; height: 96px; border-radius: 10px;">
+              <div class="row">
+                <div class="col-3">
+                  <img src="../assets/images/Mask_Group.png" style="width: 67px; object-fit: contain; padding-top: 15px;" alt="mask_group">  
+                </div>
+                <div class="col-5">
+                  <div class="py-2">
+                    <span style="font-size: 15.67px; color: #525252; line-height: 18.36px; font-weight: 500;">Bag of Laundry</span>
+                  </div>
+                  <div>
+                    <span style="font-size: 9.97px; color: #535353; font-weight: 400; line-height: 11.68px">Total Order</span>
+                  </div>
+                  <span style="color: #0099EE; font-weight: 700; font-size: 14.85px; line-height: 17.4px;">$ 180.00</span>
+                </div>
+                <div class="col-4">
+                  <div class="p-4" style="background: linear-gradient(to bottom right ,#2D9CDB, #0099EE, #CAECFF); width: 90px; height: 96px;">
+                    <img src="../assets/images/note.png" style="width: 46px; height: 46px;" alt="invoice_img">
+                    <p style="font-size: 10.45px; line-height: 12.24px; font-weight: 500; color: #ffff; padding-top: 6px;">INVOICE</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
 </template>
@@ -53,9 +83,4 @@
     justify-content: center;
   }
 
-  .content-card {
-    margin-top: -10rem;
-    /* margin-left: 36px;
-    margin-right: 37px; */
-  }
 </style>
