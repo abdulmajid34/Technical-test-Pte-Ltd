fa3de24b65e65b61d95cd09a6705de01fbfef019
abdulmajid <majid@kebun.tech>
2023-08-15 12:12:27 +0700

    feat: added layout previous order

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -61,6 +61,18 @@
             </div>
           </div>
         </div>
+        <div class="row justify-content-center" style="margin-top: 39px;">
+          <div class="col-md-4">
+            <h2 class="mx-4" style="color: #3B97CB; font-weight: 700; font-size: 15.23px; line-height: 17.85px;">PREVIOUS ORDER</h2>
+          </div>
+        </div>
+        <div class="row justify-content-center">
+          <div class="col-md-4">
+            <div class="mx-4">
+              <img src="../assets/images/Group_21.png" style="width: 302px; height: 214px;" alt="group_21">
+            </div>
+          </div>  
+        </div>
       </div>
     </section>
 </template>
